39ae58211843800e18e6448515cd23b88686c596
Trinh Minh Triet <contact@trinhminhtriet.com>
2024-12-12 23:12:02 +0700

    Refactor main function to use async and add command-line argument for User-Agent

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use base64;
+use clap::{Arg, Command};
+use reqwest::header::{HeaderMap, HeaderValue, USER_AGENT};
+use serde_json::Value;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let help = Arg::new("agent")
+        .short('A')
+        .long("agent")
+        .help("User-Agent name");
+
+    Ok(())
 }
